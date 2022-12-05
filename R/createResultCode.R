915f226fd620d46a1d7edbcf1724df4e1de7456f
denisroyer <37148577+denisroyer@users.noreply.github.com>
2022-12-05 21:49:29 +0100

    Added roxygen Comments

--- R/createResultCode.R
+++ R/createResultCode.R
@@ -1,6 +1,20 @@
 library(tidyverse)
 library(qrcode)
 
+#' Generates Result QR-Code
+#'
+#' @param obj An Oject to be hashed.
+#' @param url An URL.
+#' @param suffix Suffix of the URL.
+#' @param site Additional URL parameters.
+#' @param debug The debug Output
+#' @return The QR-Code.
+#' @export
+#' @import tidyverse
+#' @import qrcode
+#' @examples
+#' createResultCode(mtcars)
+
 createResultCode <- function(obj, url = "http://r.myasterisk.de/LSG/", suffix = ".html", site = "", debug = FALSE) {
   myHash <- rlang::hash(obj)
   if (debug == TRUE) print(myHash)
