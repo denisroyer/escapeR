e2b79b79e68e68731ce5adea559a2a16456c0d09
denisroyer <37148577+denisroyer@users.noreply.github.com>
2022-12-05 20:09:06 +0100

    added QR Function

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -15,4 +15,16 @@
 
 hello <- function() {
   print("Hello, world!")
+}
+
+library(tidyverse)
+library(qrcode)
+
+createResultCode <- function(obj, url = "http://r.myasterisk.de/LSG/", suffix = ".html", site = "", debug = FALSE) {
+  myHash <- rlang::hash(obj)
+  if (debug == TRUE) print(myHash)
+  #userName <- paste("&user=",Sys.info()["user"], sep="")
+  myCode <- paste(url, site , myHash, suffix, sep = "")
+  if (debug == TRUE) print(myCode)
+  return(qr_code(myCode))
 }
