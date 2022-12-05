c195e1201ca29080be22c3cc25fd61281fb5da75
denisroyer <37148577+denisroyer@users.noreply.github.com>
2022-12-05 22:19:31 +0100

    added QR Code Function

--- R/createResultCode.R
+++ R/createResultCode.R
@@ -23,3 +23,26 @@ createResultCode <- function(obj, url = "http://r.myasterisk.de/LSG/", suffix = 
   if (debug == TRUE) print(myCode)
   return(qr_code(myCode))
 }
+
+#' Generates Result QR-Code as Images
+#'
+#' @param obj An Oject to be hashed.
+#' @param url An URL.
+#' @param suffix Suffix of the URL.
+#' @param site Additional URL parameters.
+#' @param debug The debug Output
+#' @return The QR-Code.
+#' @export
+#' @import tidyverse
+#' @import qrcode
+#' @examples
+#' createResultCode(mtcars)
+
+createResultQRCode <- function(obj, url = "http://r.myasterisk.de/LSG/", suffix = ".html", site = "", debug = FALSE) {
+  myHash <- rlang::hash(obj)
+  if (debug == TRUE) print(myHash)
+  #userName <- paste("&user=",Sys.info()["user"], sep="")
+  myCode <- paste(url, site , myHash, suffix, sep = "")
+  if (debug == TRUE) print(myCode)
+  plot(qr_code(myCode))
+}
